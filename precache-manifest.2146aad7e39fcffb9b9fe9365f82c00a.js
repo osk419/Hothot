d39a2d81a9cf0ae6f67b3cd277dcd3336fe7f2bd
Oskar <osk419@gmail.com>
2020-02-17 17:15:12 +0100

    Updates

---
+++ precache-manifest.2146aad7e39fcffb9b9fe9365f82c00a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6472fcd3a9c8e6eb00719c024adb6ccf",
+    "url": "/Hothot/index.html"
+  },
+  {
+    "revision": "ba8c1a7508e459a2fb9a",
+    "url": "/Hothot/static/css/main.e0e3a233.chunk.css"
+  },
+  {
+    "revision": "68684699dbb825ea6bd3",
+    "url": "/Hothot/static/js/2.b6d122d8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Hothot/static/js/2.b6d122d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba8c1a7508e459a2fb9a",
+    "url": "/Hothot/static/js/main.422bff08.chunk.js"
+  },
+  {
+    "revision": "d854a159ff10ff5735af",
+    "url": "/Hothot/static/js/runtime-main.3e4dd1f1.js"
+  },
+  {
+    "revision": "10b8861c3e0974967924f68fe24cd196",
+    "url": "/Hothot/static/media/heart-filled.10b8861c.svg"
+  },
+  {
+    "revision": "8bd8e7e0073f88d9d62a1f3276636e08",
+    "url": "/Hothot/static/media/heart.8bd8e7e0.svg"
+  },
+  {
+    "revision": "3d51765ded02d577b53416713a6973da",
+    "url": "/Hothot/static/media/hothot.3d51765d.png"
+  },
+  {
+    "revision": "fdcb410f42c6bece4180a416b2395619",
+    "url": "/Hothot/static/media/kiss.fdcb410f.svg"
+  }
+]);
